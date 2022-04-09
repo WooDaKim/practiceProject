760a7a0cd49f8f92e127bea1d222c053ce7817a0
wooda <woosskimgit@gmail.com>
2022-04-09 22:06:43 +0900

    Initial commit

---
+++ src/test/java/com/example/RecordMe/RecordMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecordMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
