339eee70c7d34aa099ca12e6bbc36d9d1cb061e7
wooda <woosskimgit@gmail.com>
2022-04-19 22:46:35 +0900

    메인화면수정

--- src/main/java/com/example/RecordMe/service/UserService.java
+++ src/main/java/com/example/RecordMe/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.RecordMe.service;
 
 import com.example.RecordMe.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
